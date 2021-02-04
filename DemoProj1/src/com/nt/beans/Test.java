bd9b2155854cb02e9465370d862dbbd83f28b854
Devnaershit <admin@DESKTOP-ENTDRRE>
2021-02-05 00:20:26 +0530

    Dev added test.java

---
+++ DemoProj1/src/com/nt/beans/Test.java
@@ -0,0 +1,13 @@
+package com.nt.beans;
+
+import com.nt.test.Utility;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Utility u = new Utility();
+		System.out.println(u.getSysDate());
+
+	}
+
+}
