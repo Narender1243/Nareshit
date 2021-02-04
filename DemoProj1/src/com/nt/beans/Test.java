6e397fbec67797ab74199cae671d5633991ee190
Devnaershit <admin@DESKTOP-ENTDRRE>
2021-02-05 00:49:37 +0530

    formated test.java

--- DemoProj1/src/com/nt/beans/Test.java
+++ DemoProj1/src/com/nt/beans/Test.java
@@ -8,9 +8,11 @@ public class Test {
 	public void y() {
 		System.out.println("Test.y()");
 	}
+
 	public void m1() {
 		System.out.println("From m1()");
 	}
+
 	public void m2() {
 		System.out.println("From m2()");
 	}
